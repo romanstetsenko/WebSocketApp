ce04a0a1ce949822c908efcade80985886e0bb81
stetsenkorp <stetsenkorp@gmail.com>
2019-04-03 00:51:57 +0200

    Add project files.

---
+++ src/WebSocketApp/Program.fs
@@ -0,0 +1,109 @@
+module WebSocketApp.App
+
+open System
+open System.IO
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Cors.Infrastructure
+open Microsoft.AspNetCore.Hosting
+open Microsoft.Extensions.Logging
+open Microsoft.Extensions.DependencyInjection
+open Giraffe
+open Giraffe.Razor
+open WebSocketApp.Models
+open Middleware
+open FSharp.Control.Tasks.V2
+open Microsoft.AspNetCore.Http
+
+// ---------------------------------
+// Web app
+// ---------------------------------
+
+let indexHandler (name : string) =
+    let greetings = sprintf "Hello %s, from Giraffe!" name
+    let model     = { Text = greetings }
+    razorHtmlView "Index" (Some model) None None
+
+let handlePostMessage =
+    fun (next : HttpFunc) (ctx : HttpContext) ->
+        task {
+            let! message = ctx.BindJsonAsync<Message>()
+            do! sendMessageToSockets message.Text
+            return! next ctx
+        }
+
+
+let webApp =
+    choose [
+        GET >=>
+            choose [
+                route "/" >=> indexHandler "world"
+                routef "/hello/%s" indexHandler
+            ]
+        POST >=>
+            choose [
+                route "/message" >=> handlePostMessage
+            ]
+        setStatusCode 404 >=> text "Not Found" ]
+
+// ---------------------------------
+// Error handler
+// ---------------------------------
+
+let errorHandler (ex : Exception) (logger : ILogger) =
+    logger.LogError(ex, "An unhandled exception has occurred while executing the request.")
+    clearResponse >=> setStatusCode 500 >=> text ex.Message
+
+// ---------------------------------
+// Config and Main
+// ---------------------------------
+
+let configureCors (builder : CorsPolicyBuilder) =
+    builder.WithOrigins("http://localhost:8080")
+           .AllowAnyMethod()
+           .AllowAnyHeader()
+           |> ignore
+
+let configureApp (app : IApplicationBuilder) =
+    let env = app.ApplicationServices.GetService<IHostingEnvironment>()
+    (match env.IsDevelopment() with
+    | true  -> app.UseDeveloperExceptionPage()
+    | false -> app.UseGiraffeErrorHandler errorHandler)
+        .UseWebSockets()
+        .UseMiddleware<WebSocketMiddleware>()
+        .UseHttpsRedirection()
+        .UseCors(configureCors)
+        .UseStaticFiles()
+        .UseGiraffe(webApp)
+
+let configureServices (services : IServiceCollection) =
+    let sp  = services.BuildServiceProvider()
+    let env = sp.GetService<IHostingEnvironment>()
+    let viewsFolderPath = Path.Combine(env.ContentRootPath, "Views")
+    services.AddRazorEngine viewsFolderPath |> ignore
+    services.AddCors() |> ignore
+    services.AddGiraffe() |> ignore
+
+let configureLogging (builder : ILoggingBuilder) =
+    builder.AddFilter(fun l -> l.Equals LogLevel.Error)
+           .AddConsole()
+           .AddDebug() |> ignore
+
+let tryGetEnv = System.Environment.GetEnvironmentVariable >> function null | "" -> None | x -> Some x
+let port = "SERVER_PORT" |> tryGetEnv |> Option.map uint16 |> Option.defaultValue 5000us
+
+[<EntryPoint>]
+let main _ =
+    let contentRoot = Directory.GetCurrentDirectory()
+    let webRoot     = Path.Combine(contentRoot, "WebRoot")
+    WebHostBuilder()
+        .UseKestrel()
+        .UseContentRoot(contentRoot)
+        .UseIISIntegration()
+        .UseWebRoot(webRoot)
+        .Configure(Action<IApplicationBuilder> configureApp)
+        .ConfigureServices(configureServices)
+        .UseUrls("http://127.0.0.1:" + port.ToString() + "/")
+        .ConfigureLogging(configureLogging)
+        .Build()
+        .Run()
+    0
---
+++ src/WebSocketApp/WebSocketMiddleware.fs
@@ -0,0 +1,106 @@
+﻿namespace WebSocketApp
+
+module Middleware =
+    open System
+    open System.Text
+    open System.Threading
+    open System.Threading.Tasks
+    open System.Net.WebSockets
+    open Microsoft.AspNetCore.Http
+    open FSharp.Control.Tasks.V2
+    
+
+    let mutable sockets = list<WebSocket>.Empty
+
+    let private addSocket sockets socket = socket :: sockets
+
+    let private removeSocket sockets socket =
+        sockets
+        |> List.choose (fun s -> if s <> socket then Some s else None)
+
+    let private sendMessage =
+        fun (socket : WebSocket) (message : string) ->
+            task {
+                let buffer = Encoding.UTF8.GetBytes(message)
+                let segment = new ArraySegment<byte>(buffer)
+
+                if socket.State = WebSocketState.Open then
+                    do! socket.SendAsync(segment, WebSocketMessageType.Text, true, CancellationToken.None)
+                else
+                    sockets <- removeSocket sockets socket
+            }
+    
+    let sendMessageToSockets =
+        fun message ->
+            task {
+                for socket in sockets do
+                    try
+                        do! sendMessage socket message
+                    with
+                        | _ -> sockets <- removeSocket sockets socket
+            }
+    
+    let keepConnectionAlive (webSocket:WebSocket) = 
+            webSocket.ReceiveAsync(new ArraySegment<byte>(), CancellationToken.None).Wait()
+            //let buffer : byte[] = Array.zeroCreate 1024
+            //while webSocket.State = WebSocketState.Open do
+                
+            //    match res.MessageType with 
+            //    | WebSocketMessageType.Binary -> 
+            //        do! webSocket.CloseOutputAsync(WebSocketCloseStatus.InvalidPayloadData, "Sorry, WebSocketMessageType.Binary isn't supported yet.", CancellationToken.None)
+            //    | WebSocketMessageType.Close -> 
+            //        ()
+            //    | WebSocketMessageType.Text ->
+            //        let msg = System.Text.Encoding.UTF8.GetString(buffer, 0, res.Count)
+            //        // publish msg
+            //        ()
+            //    | _ -> ()
+
+    type WebSocketMiddleware(next : RequestDelegate) =
+        member __.Invoke(ctx : HttpContext) =
+            //async {
+            //    if ctx.Request.Path = PathString("/ws") then
+            //        match ctx.WebSockets.IsWebSocketRequest with
+            //        | true ->
+            //            let! webSocket = ctx.WebSockets.AcceptWebSocketAsync() |> Async.AwaitTask
+            //            sockets <- addSocket sockets webSocket
+
+            //            let buffer : byte[] = Array.zeroCreate 4096
+            //            let! ct = Async.CancellationToken
+                        
+            //            webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), ct)
+            //            |> Async.AwaitTask
+            //            |> ignore
+
+            //        | false -> ctx.Response.StatusCode <- 400
+            //    else
+            //        next.Invoke(ctx) |> Async.AwaitTask |> ignore
+            //} |> Async.StartAsTask :> Task
+            task {
+                if ctx.Request.Path = PathString("/ws") then
+                    match ctx.WebSockets.IsWebSocketRequest with
+                    | true ->
+                        //ctx.Response.StatusCode <- 200
+                        //while true do
+                        let! webSocket = ctx.WebSockets.AcceptWebSocketAsync()
+                        sockets <- addSocket sockets webSocket
+                        let buffer : byte[] = Array.zeroCreate 4096
+                        let! res = webSocket.ReceiveAsync(new Memory<byte>(buffer), CancellationToken.None)
+                        keepConnectionAlive webSocket
+                        
+
+                        //let sendBuffer = System.Text.Encoding.UTF8.GetBytes(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fffffff"))
+                        //do! webSocket.SendAsync(new ArraySegment<byte>(sendBuffer), WebSocketMessageType.Text, true, CancellationToken.None)
+                        //let buffer : byte[] = Array.zeroCreate 4096
+                        
+                        //let! re = webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None)
+                        //re |> ignore
+                        //()
+                        //for i in [1..100000] do
+                        //    let sendBuffer = System.Text.Encoding.UTF8.GetBytes(string i + DateTime.Now.ToString(": yyyy-MM-dd HH:mm:ss.fffffff"))
+                        //    do! webSocket.SendAsync(new ReadOnlyMemory<byte>(sendBuffer), WebSocketMessageType.Text, true, CancellationToken.None)
+                    | false -> 
+                        ctx.Response.StatusCode <- 400
+                else
+                    do! next.Invoke(ctx) 
+            }

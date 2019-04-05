507b22360c7d5f57bca4cbca286f84c0ac9f6ca6
stetsenkorp <stetsenkorp@gmail.com>
2019-04-06 00:21:56 +0200

    gut

---
+++ Client/ChatClientBuilder.fs
@@ -0,0 +1,45 @@
+﻿namespace Client
+
+module ChatClientBuilder =
+    open System.Net
+    open System
+    open Orleans
+    open Orleans.Configuration
+    open Orleans.Hosting
+    open Orleans.Runtime
+    open Orleankka.Client
+    open Messages
+    open Client.ChatClient
+
+
+    let DemoClusterId = "localhost-demo"
+    let DemoServiceId = "localhost-demo-service"
+    let LocalhostGatewayPort = 30000
+    let LocalhostSiloAddress = IPAddress.Loopback
+    let create () =
+        
+        printfn "Please wait until Chat Server has completed boot and then press enter. \n"
+        Console.ReadLine() |> ignore
+
+        let cb = new ClientBuilder()
+        cb.Configure<ClusterOptions>(fun (options:ClusterOptions) -> options.ClusterId <- DemoClusterId; options.ServiceId <- DemoServiceId) |> ignore
+        cb.UseStaticClustering(fun (options:StaticGatewayListProviderOptions) -> options.Gateways.Add(IPEndPoint(LocalhostSiloAddress, LocalhostGatewayPort).ToGatewayUri())) |> ignore
+        cb.AddSimpleMessageStreamProvider("sms") |> ignore
+        cb.ConfigureApplicationParts(fun x -> x.AddApplicationPart(typeof<IChatUser>.Assembly).WithCodeGeneration() |> ignore) |> ignore
+        cb.UseOrleankka() |> ignore
+
+        let client = cb.Build()
+        client.Connect().Wait()
+      
+        printfn "Enter your user name..."
+        let userName = Console.ReadLine();
+
+        printfn "Enter a room which you'd like to join..."
+        let roomName = Console.ReadLine();
+
+        let system = client.ActorSystem()
+        let t = startChatClient system userName roomName 
+        t.Wait()
+
+        Console.ReadLine() |> ignore  
+        system 
